967cd2681dcb84d03f15672cb0bd0d5eec705aa2
shlomiatia <shlomiatia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2012-01-22 10:17:11 +0000

    Created new h files

--- Temporal/Temporal/Base/Base.h
+++ Temporal/Temporal/Base/Base.h
@@ -2,40 +2,8 @@
 
 #include <stdio.h> // NULL
 
-static const float  PI = 3.14159265358979f;
-
-static float toRadians(float angle) { return angle * PI / 180; }
-static float toDegrees(float angle) { return angle * 180 / PI; }
-
-namespace Temporal
-{
-	namespace Orientation
-	{ 
-		enum Type
-		{
-			LEFT = -1,
-			RIGHT = 1 
-		};
-	}
-
-	namespace Direction
-	{
-		enum Type
-		{
-			TOP = 1,
-			BOTTOM = 2,
-			FRONT = 4,
-			BACK = 8
-		};
-
-		static const Direction::Type NONE = (Direction::Type)(0);
-	}
-	inline Direction::Type operator|(Direction::Type a, Direction::Type b) { return (Direction::Type)((int)a | (int)b); }
-	inline Direction::Type operator&(Direction::Type a, Direction::Type b) { return (Direction::Type)((int)a & (int)b); }
-	inline bool match(Direction::Type direction, Direction::Type positive) { return (direction & positive) == positive; }
-	inline bool match(Direction::Type direction, Direction::Type positive, Direction::Type negative) { return match(direction, positive) && !(direction & negative); }
-}
-
+#include "Math.h"
+#include "Enums.h"
 #include "Color.h"
 #include "Vector.h"
 #include "Line.h"
