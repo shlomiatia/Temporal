c86338db46f5136d7591b69e3ee007ed050c9c8f
shlomiatia <shlomiatia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2012-05-17 09:29:29 +0000

    Stored a global map of hash to string

--- trunk/Temporal/Temporal/Hash.h
+++ trunk/Temporal/Temporal/Hash.h
@@ -1,21 +1,44 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <unordered_map>
+
 namespace Temporal
 {
-	// TODO: Global string list
 	class Hash
 	{
 	public:
 		static const Hash INVALID;
 
-		explicit Hash(const char* string);
+		explicit Hash(const char* string);		
 		explicit Hash(unsigned int value) : _value(value) {}
 
-		operator unsigned int() const { return _value; }
+		operator unsigned int(void) const { return _value; }
 		bool operator==(const Hash& other) const { return _value == other._value; }
 	private:
 		unsigned int _value;
+	};
+
+	typedef std::unordered_map<const Hash, const char*> HashCollection;
+	typedef HashCollection::const_iterator HashIterator;
+
+	class HashToString
+	{
+	public:
+		static HashToString& get(void)
+		{
+			static HashToString instance;
+			return instance;
+		}
+
+		void save(const Hash& key, const char* value);
+		const char* getString(const Hash& hash) const;
+	private:
+		HashCollection _hashStrings;
+
+		HashToString(void) {}
+		HashToString(const HashToString&);
+		HashToString& operator=(const HashToString&);
 	};
 }
 
