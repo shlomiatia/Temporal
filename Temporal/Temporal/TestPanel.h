fa5c6e88b020c4df990b91c475435cf07f1279cd
shlomiatia <shlomiatia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2012-08-11 09:13:23 +0000

    orginized tasks

--- Temporal/Temporal/TestPanel.h
+++ Temporal/Temporal/TestPanel.h
@@ -9,34 +9,48 @@ namespace Temporal
 	// ASSUME: When trying to walk on a flat floor where the ceiling is sloped so there's no room, the entity will go down a bit *\
 	// ASSUME: When trying to walk on a sloped floor where the ceiling is flat so there's no room, the entity will shake */
 
-	// TODO: Normalize positions - attch to scene graph
 	// TODO: Minimize casting
 	// TODO: message utils
 	// TODO: sprite size
 
-	// TODO: Check sensor each movement step PHYSICS
-	// TODO: Insert segments to grid using grid traversale algorithm PHYSICS
-	// TODO: Store YABP in grid PHYSICS
-	
-	// TODO: Deal with component order ENTITIES
+	// General
+	// TODO: Text
+	// TODO: Files
+	// TODO: Resources
+	// TODO: Input
+	// TODO: systems
+	// TODO: games states/panels
+	// TODO: animations
+	// TODO: Deal with component order
 
-	// TODO: Consider transition type in navigation edge cost  AI
-	// TODO: Fix huristics:  Take into account position, need for turning, and several edges to the same platform AI
-	// TODO: Fault tolerance in navigator AI
+	// Physics
+	// TODO: moving platforms
+	// TODO: Sensor/Button
+	// TODO: Check sensor each movement step
+	// TODO: Insert segments to grid using grid traversale algorithm
+	// TODO: Store YABP in grid
 
-	// TODO: Create arbitrary points on entity (draw position, eyes) GRAPHICS
-	// TODO: Gradual hang & descend GRAPHICS
-	// TODO: Test against top, center bottom in sight GRAPHICS
-	// TODO: Gradual temporal echo merge GRAPHICS
-	// TODO: Soft shadows GRAPHICS
+	// Graphics
+	// TODO: Scene graph attachment (draw position, eyes)
+	// TODO: Gradual hang & descend
+	// TODO: Test against top, center bottom in sight
+	// TODO: Gradual temporal echo merge
+	// TODO: Soft shadows
+
+	// AI
+	// TODO: Consider transition type in navigation edge cost
+	// TODO: Fix huristics:  Take into account position, need for turning, and several edges to the same platform
+	// TODO: Fault tolerance in navigator
 	 
-	// TODO: Enable/Disable Sensors PERFORMACE
-	// TODO: Request sight PERFORMANCE
-	// TODO: Cache ray properties PERFORMANCE
-	// TODO: Do not process collision twice PERFORMANCE
-	// TODO: Less temporal echo memory PERFORMANCE
-	// TODO: Delete spritesheets, singletons, static stl containers PERFORMANCE
-	// TODO: Cache shadows/display list/FBO/circle grid/light culling/save texture PERFORMANCE
+	// Performance
+	// TODO: Enable/Disable Sensors
+	// TODO: Request sight
+	// TODO: Cache ray properties/global shape
+	// TODO: Do not process collision twice
+	// TODO: Less temporal echo memory
+	// TODO: Culling
+	// TODO: Delete spritesheets, singletons, static stl containers
+	// TODO: Cache shadows/display list/FBO/circle grid/light culling/save texture
 	class TestPanel : public BasePanel
 	{
 	public:
