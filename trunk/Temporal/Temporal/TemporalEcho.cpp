7a21b8d7ad735b1a8a4579d8445af37015b3ecfb
shlomiatia <shlomiatia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2015-08-09 18:49:57 +0000

    temporal echo cooldown

--- trunk/Temporal/Temporal/TemporalEcho.cpp
+++ trunk/Temporal/Temporal/TemporalEcho.cpp
@@ -12,6 +12,7 @@ namespace Temporal
 	const Hash RENDERER_TYPE = Hash("renderer");
 	const Hash ANIMATOR_TYPE = Hash("animator");
 	const float TemporalEcho::ECHO_READY_TIME = 2.0f;
+	const float TemporalEcho::ECHO_COOLDOWN_TIME = 1.0f;
 
 	HashList TemporalEcho::getFilter() const
 	{
@@ -33,36 +34,46 @@ namespace Temporal
 
 	void TemporalEcho::update(float framePeriod)
 	{
-		if(!_echoReady)
+		if (_cooldown)
 		{
-			if (_saveTimer.getElapsedTime() > ECHO_READY_TIME)
+			_cooldownTimer.update(framePeriod);
+			if (_cooldownTimer.getElapsedTime() > ECHO_COOLDOWN_TIME)
 			{
-				_echoReady = true;
-				_loadTimer.reset();
+				_cooldown = false;
 			}
 		}
-		if(_echoReady)
+		if (!_cooldown)
 		{
-			_loadTimer.update(framePeriod);
-			TemporalEchoDataIterator first = _echoesData.begin();
-			if ((**first).getTime() <= _loadTimer.getElapsedTime())
+			if (!_echoReady)
 			{
-				const Stream* deserialization = (**first).getStream();
-				BinaryDeserializer deserializer(deserialization);
-				deserializer.serialize("entity", _echo);
-				delete deserialization;
-				_echoesData.erase(first);
-				float alpha = 0.2f;
-				_echo->handleMessage(Message(MessageID::SET_ALPHA, &alpha));
+				if (_saveTimer.getElapsedTime() > ECHO_READY_TIME)
+				{
+					_echoReady = true;
+					_loadTimer.reset();
+				}
 			}
+			if (_echoReady)
+			{
+				_loadTimer.update(framePeriod);
+				TemporalEchoDataIterator first = _echoesData.begin();
+				if ((**first).getTime() <= _loadTimer.getElapsedTime())
+				{
+					const Stream* deserialization = (**first).getStream();
+					BinaryDeserializer deserializer(deserialization);
+					deserializer.serialize("entity", _echo);
+					delete deserialization;
+					_echoesData.erase(first);
+					float alpha = 0.2f;
+					_echo->handleMessage(Message(MessageID::SET_ALPHA, &alpha));
+				}
+			}
+			_saveTimer.update(framePeriod);
+			Stream* serialization = new MemoryStream();
+			BinarySerializer serializer(serialization);
+			serializer.serialize("entity", getEntity());
+			TemporalEchoData* data = new TemporalEchoData(_saveTimer.getElapsedTime(), serialization);
+			_echoesData.push_back(data);
 		}
-		_saveTimer.update(framePeriod);
-		Stream* serialization = new MemoryStream();
-		BinarySerializer serializer(serialization);
-		serializer.serialize("entity", getEntity());
-		TemporalEchoData* data = new TemporalEchoData(_saveTimer.getElapsedTime(), serialization);
-		_echoesData.push_back(data);
-		
 	}
 
 	void TemporalEcho::disableEcho()
@@ -89,6 +100,8 @@ namespace Temporal
 			deserializer.serialize("entity", getEntity());
 			disableEcho();
 			getEntity().handleMessage(Message(MessageID::POST_LOAD));
+			_cooldown = true;
+			_cooldownTimer.reset();
 		}
 	}
 
