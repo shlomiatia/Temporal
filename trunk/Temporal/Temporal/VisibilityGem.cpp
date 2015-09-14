1b1991222b137372344e32a4fb4a0a94809b9270
ShlomiAtia <ShlomiAtia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2015-09-14 08:47:05 +0000

    auto color enemy period

--- trunk/Temporal/Temporal/VisibilityGem.cpp
+++ trunk/Temporal/Temporal/VisibilityGem.cpp
@@ -10,13 +10,26 @@ namespace Temporal
 
 	void VisibilityGem::handleMessage(Message& message)
 	{
-		if (message.getID() == MessageID::ENTITY_INIT) 
+		if (message.getID() == MessageID::TEMPORAL_PERIOD_CHANGED)
 		{
 			_isVisible = false;
 		}
-		if (message.getID() == MessageID::SET_LIT)
+		else if (message.getID() == MessageID::SET_LIT)
 		{
 			_isLit = getBoolParam(message.getParam());
+		}
+		else if (message.getID() == MessageID::IS_VISIBLE)
+		{
+			_isVisible = _isLit && !_isCovered;
+			message.setParam(&_isVisible);
+		}
+		else if (message.getID() == MessageID::SENSOR_START)
+		{
+			const SensorParams& params = getSensorParams(message.getParam());
+			if (params.getSensorId() == ACTIVATE_SENSOR_ID)
+			{
+				_isCovered = false;
+			}
 		}
 		else if (message.getID() == MessageID::SENSOR_SENSE)
 		{
@@ -25,19 +38,10 @@ namespace Temporal
 			{
 				const OBB& body = params.getContact().getSource().getGlobalShape();
 				const OBB& cover = params.getContact().getTarget().getGlobalShape();
-				
+
 				if (body.getLeft() >= cover.getLeft() && body.getRight() <= cover.getRight() && body.getBottom() >= cover.getBottom() && body.getTop() <= cover.getTop())
-					_isVisible = false;
+					_isCovered = true;
 			}
-		}
-		else if (message.getID() == MessageID::UPDATE)
-		{
-			_isVisible = true;
-		}
-		else if (message.getID() == MessageID::IS_VISIBLE)
-		{
-			_isVisible &= _isLit;
-			message.setParam(&_isVisible);
 		}
 	}
 }
