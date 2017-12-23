3465c7d945cbd68fbce3dcfb17363d5714ac5939
shlomiatia <shlomiatia@3a4dcf1f-5df4-4445-a543-cd44aa9e3a8f>
2017-12-23 19:01:35 +0000

    more

--- Temporal/ComponentEditor.h
+++ Temporal/ComponentEditor.h
@@ -15,28 +15,30 @@ namespace Temporal
 	public:
 		ComponentEditorSerializer(ComponentEditor& componentEditorSerializer) : BaseSerializer((Stream*)0), _componentEditorSerializer(componentEditorSerializer) {}
 
-		void serialize(const char* key, int& value);
-		void serialize(const char* key, unsigned int& value);
+		void serialize(const char* key, int& value) {};
+		void serialize(const char* key, unsigned int& value) {};
 		void serialize(const char* key, float& value);
 		void serialize(const char* key, bool& value);
 		void serialize(const char* key, Hash& value);
-		void serialize(const char* key, Timer& value);
-		void serializeRadians(const char* key, float& value);
-		void serialize(const char* key, std::string& value);
-		void preSerialize(const char* key);
-		void postSerialize(const char* key);
+		void serialize(const char* key, Timer& value) {};
+		void serializeRadians(const char* key, float& value) {};
+		void serialize(const char* key, std::string& value) {};
 		SerializationDirection::Enum type() { return SerializationDirection::SERIALIZATION; };
 
 		template<class T>
 		void serialize(const char* key, T*& value)
 		{
-			if (SerializationAccess::shouldSerialize(value))
-			{
-				SerializationAccess::modifyKey(key, value);
-				preSerialize(key);
-				SerializationAccess::serialize(key, value, *this);
-				postSerialize(key);
-			}
+			if (!SerializationAccess::shouldSerialize(value))
+				return;
+
+			SerializationAccess::modifyKey(key, value);
+
+			if (strcmp(key, "sensor") == 0 || strcmp(key, "animator") == 0 || strcmp(key, "renderer") == 0 || strcmp(key, "dynamic-body") == 0 || strcmp(key, "navigator") == 0)
+				return;
+
+			preSerialize(key);
+			SerializationAccess::serialize(key, value, *this);
+			postSerialize(key);
 		}
 
 		template<class T>
@@ -65,10 +67,15 @@ namespace Temporal
 		}
 
 	private:
+		void preSerialize(const char* key);
+		void postSerialize(const char* key);
+
 		ComponentEditor& _componentEditorSerializer;
 		std::vector<std::string> _prefixes;
 
-		void hashChanged(const char* s, Hash& hash);
+		void hashChanged(const char* controlValue, Hash& componentValue);
+		void boolChanged(bool controlValue, bool& componentValue);
+		void floatChanged(const char* controlValue, float& componentValue);
 	};
 
 	class ComponentEditor : public ToolComponent
